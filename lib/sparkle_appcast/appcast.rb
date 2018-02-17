681bc4a95de2939c54d16b0336ca0add6a6b9af5
Yoshimasa Niwa <niw@niw.at>
2018-02-17 15:30:16 -0800

    Use bundle_info.

--- lib/sparkle_appcast/appcast.rb
+++ lib/sparkle_appcast/appcast.rb
@@ -18,21 +18,21 @@ module SparkleAppcast
 
     def rss_params
       {
-        channel_link: archive.feed_url,
+        channel_link: archive.bundle_info[:feed_url],
         title: title,
         description: release_note.html,
         publish_date: archive.created_at,
         url: url,
         length: archive.size,
-        version: archive.bundle_version,
-        short_version_string: archive.bundle_short_version_string,
-        minimum_system_version: archive.minimum_system_version,
+        version: archive.bundle_info[:bundle_version],
+        short_version_string: archive.bundle_info[:bundle_short_version_string],
+        minimum_system_version: archive.bundle_info[:minimum_system_version],
         dsa_signature: dsa_signature
       }.merge(params)
     end
 
     def title
-      "#{archive.bundle_name} #{archive.bundle_short_version_string} (#{archive.bundle_version})"
+      "#{archive.bundle_info[:bundle_name]} #{archive.bundle_info[:bundle_short_version_string]} (#{archive.bundle_info[:bundle_version]})"
     end
 
     def dsa_signature
--- lib/sparkle_appcast/archive.rb
+++ lib/sparkle_appcast/archive.rb
@@ -1,5 +1,4 @@
 require "tmpdir"
-require "plist"
 
 module SparkleAppcast
   class Archive
@@ -7,26 +6,6 @@ module SparkleAppcast
 
     def initialize(path)
       @path = File.expand_path(path)
-    end
-
-    def bundle_name
-      info_plist["CFBundleDisplayName"] || info_plist["CFBundleName"]
-    end
-
-    def bundle_version
-      info_plist["CFBundleVersion"]
-    end
-
-    def bundle_short_version_string
-      info_plist["CFBundleShortVersionString"]
-    end
-
-    def minimum_system_version
-      info_plist["LSMinimumSystemVersion"]
-    end
-
-    def feed_url
-      info_plist["SUFeedURL"]
     end
 
     def created_at
@@ -41,28 +20,9 @@ module SparkleAppcast
       File.binread(path)
     end
 
-    private
-
-    # TODO: Support localizable string.
-
-    def info_plist
-      @info_plist ||= Dir.mktmpdir do |tmpdir_path|
-        case File.basename(path)
-        when /\.zip\z/i
-          Kernel.system("/usr/bin/ditto", "-x", "-k", path, tmpdir_path)
-        when /\.tar\z/i
-          Kernel.system("/usr/bin/tar", "-x", "-f", path, "-C", tmpdir_path)
-        when /\.tar\.gz\z/i, /\.tgz\z/i, /\.tar\.xz\z/i, /\.txz\z/i, /\.tar\.lzma\z/i
-          Kernel.system("/usr/bin/tar", "-x", "-z", "-f", path, "-C", tmpdir_path)
-        when /\.tar\.bz2\z/i, /\.tbz\z/i
-          Kernel.system("/usr/bin/tar", "-x", "-j", "-f", path, "-C", tmpdir_path)
-        else
-          raise NotImplementedError.new("Disk image support is not implemented yet.")
-        end
-
-        unless $?.success?
-          raise RuntimeError.new("Failed to expand archive: #{path}")
-        end
+    def bundle_info
+      @bundle_info ||= Dir.mktmpdir do |tmpdir_path|
+        unarchive!(tmpdir_path)
 
         app_paths = Dir.glob(File.join(tmpdir_path, "*.app"), File::FNM_CASEFOLD)
         if app_paths.size == 0
@@ -71,9 +31,29 @@ module SparkleAppcast
           raise RuntimeError.new("Found multiple application bundles: #{app_paths.map{|path| File.basename(path)}}")
         else
           app_path = app_paths.first
-          info_plist_path = File.join(app_path, "Contents", "Info.plist")
-          Plist.parse_xml(info_plist_path)
+          Bundle.new(app_path).info
         end
+      end
+    end
+
+    private
+
+    def unarchive!(destination_path)
+      case File.basename(path)
+      when /\.zip\z/i
+        Kernel.system("/usr/bin/ditto", "-x", "-k", path, destination_path)
+      when /\.tar\z/i
+        Kernel.system("/usr/bin/tar", "-x", "-f", path, "-C", destination_path)
+      when /\.tar\.gz\z/i, /\.tgz\z/i, /\.tar\.xz\z/i, /\.txz\z/i, /\.tar\.lzma\z/i
+        Kernel.system("/usr/bin/tar", "-x", "-z", "-f", path, "-C", destination_path)
+      when /\.tar\.bz2\z/i, /\.tbz\z/i
+        Kernel.system("/usr/bin/tar", "-x", "-j", "-f", path, "-C", destination_path)
+      else
+        raise NotImplementedError.new("Disk image support is not implemented yet.")
+      end
+
+      unless $?.success?
+        raise RuntimeError.new("Failed to expand archive: #{path}")
       end
     end
   end
